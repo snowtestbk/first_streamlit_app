208d5d795c838156634203c44c6c7f07f2f7c768
Balamurugan Kannaiyan <109117815+snowtestbk@users.noreply.github.com>
2022-07-11 23:27:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.title('Omega 3 & Blueberry Oatmeal')
