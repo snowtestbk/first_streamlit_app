b53e652714aba6f98d3980ce702cb7b184e6673e
Balamurugan Kannaiyan <109117815+snowtestbk@users.noreply.github.com>
2022-07-11 23:25:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner🍽')
+streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.title('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.title('🥗 Kale, Spinach & Rocket Smoothie 🍹 ')
-streamlit.title('🥚 Hard-Boiled Free-Range Egg')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie ')
+streamlit.title('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
