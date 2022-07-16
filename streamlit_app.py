f855691766da9612c12fb9ad08ba8d3e044431a2
Balamurugan Kannaiyan <109117815+snowtestbk@users.noreply.github.com>
2022-07-16 00:00:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
